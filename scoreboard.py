16d5d92f6f44e44d53fd854ad5a5007b2731e3ab
gopi-fsui <gopi.fsui@gmail.com>
2026-03-12 02:03:57 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,1 +1,31 @@
+FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.level = 0
+        self.penup()
+        self.goto(-280,260)
+        self.increase_level()
+
+    def increase_level(self):
+        self.clear()
+        self.level += 1
+        self.write(f"Level: {self.level}",align="left",font=FONT)
+
+    def game_over(self):
+        self.goto(-90,40)
+        self.fillcolor("red")
+        self.pencolor("black")
+        self.begin_fill()
+        for _ in range(2):
+            self.fd(180)
+            self.right(90)
+            self.fd(80)
+            self.right(90)
+        self.end_fill()
+        self.goto(0,-10)
+        self.write(f"Game over!!",align="center",font=FONT)
 
