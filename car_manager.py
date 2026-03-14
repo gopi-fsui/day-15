a50399a9e95c2af217b2e83ee38c4e4b665d53b3
gopi-fsui <gopi.fsui@gmail.com>
2026-03-14 23:29:03 +0530

    Update car_manager.py

--- car_manager.py
+++ car_manager.py
@@ -1,50 +1,41 @@
 import math
-
 COLORS = ["red", "orange", "skyblue", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 from turtle import Turtle
 import random
-
-
 class CarManager:
     def __init__(self):
         self.cars = []
         self.move_distance = STARTING_MOVE_DISTANCE
-        for x in range(-280,280,random.choice([30,40])):
+        for x in range(-300,300,random.choice([30,40])):
             self.add_cars(x)
-
-
+            
     def add_cars(self,x_value):
         car_count = random.randint(0, int(self.move_distance/5))
         for _ in range(car_count):
             new_car = Turtle("square")
             new_car.penup()
-            new_car.shapesize(1, 2.5, 1)
+            new_car.shapesize(1, 2, 1)
             new_car.color(random.choice(COLORS))
             # new_car.goto(x_value, (random.randint(-25,25))*10)
             new_car.goto(x_value, random.randint(-25,25)*10)
             self.cars.append(new_car)
-
+            
     def move_forward(self):
         for car in self.cars:
             car.goto(car.xcor()-self.move_distance,car.ycor())
-
         if random.random() < .25:
-            self.add_cars(280)
-
-
+            self.add_cars(320)
+         
     def increase_speed(self):
         self.move_distance += MOVE_INCREMENT
-
-
+        
     def turtle_acc(self,player):
         for car in self.cars:
-            if abs(car.ycor()- player.ycor()) < 20 and math.ceil(player.distance(car)) < 30:
-                print(f"first one {car.ycor()} - {player.ycor()} = {abs(car.ycor()- player.ycor())} and dis is {math.ceil(player.distance(car))}")
+            if abs(car.ycor()- player.ycor()) < 15 and math.ceil(player.distance(car)) < 30:
                 return True
-            elif math.ceil(player.distance(car)) < 24:
-                print(f"Second - {math.ceil(player.distance(car))}")
+            elif math.ceil(player.distance(car)) < 25:
                 return True
-
         return False
+        
