432f0e2949381729a04d122d37cdb862caddf8b7
gopi-fsui <gopi.fsui@gmail.com>
2026-03-14 23:29:56 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,35 +4,29 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-
-
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
-
-
 player = Player()
 scoreboard = Scoreboard()
 cars = CarManager()
 
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
 screen.listen()
 screen.onkey(player.move_up,"Up")
-
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    cars.move_forward()
+
     if player.ycor() > player.finish_line:
         player.reset_pos()
         scoreboard.increase_level()
-        cars.increase_speed()
-        # cars.mo
+        cars.increase_speed()~!
 
     if cars.turtle_acc(player):
         game_is_on = False
         scoreboard.game_over()
-
+    cars.move_forward()
 
 screen.mainloop()
